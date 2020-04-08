ed400ecf93a27d8270d3c7ebaa3d813c52796318
Dillon Huff <dillonhuff@gmail.com>
2020-04-08 00:08:30 -0700

    Pulled out helpers

--- src/camera_pipeline.cpp
+++ src/camera_pipeline.cpp
@@ -8,6 +8,20 @@
 
 #ifdef __USE_HALIDE__
 
+Halide::Buffer<float>
+sensorDataToHalide(CameraSensorData<float>* raw_data,
+    const int width,
+    const int height) {
+
+  Halide::Buffer<float> input(width, height);
+  for (int row = 0; row < height; row++) {
+    for (int col = 0; col < width; col++) {
+      input(col, row) = raw_data->data(row, col);
+    }
+  }
+  return input;
+}
+
 std::unique_ptr<Image<RgbPixel>> rgbImageFromHalide(Halide::Buffer<float>& output) {
   std::unique_ptr<Image<RgbPixel>> image(new Image<RgbPixel>(output.width(), output.height()));
 
@@ -54,12 +68,8 @@ std::unique_ptr<Image<RgbPixel>> CameraPipeline::ProcessShot() const {
 
 #ifdef __USE_HALIDE__
   std::cout << "Using Halide pipeline" << std::endl;
-  Halide::Buffer<float> input(width, height);
-  for (int row = 0; row < height; row++) {
-    for (int col = 0; col < width; col++) {
-      input(col, row) = raw_data->data(row, col);
-    }
-  }
+  Halide::Buffer<float> input =
+    sensorDataToHalide(raw_data.get(), width, height);
 
   std::cout << "Wrote Halide input" << std::endl;
 
@@ -83,23 +93,6 @@ std::unique_ptr<Image<RgbPixel>> CameraPipeline::ProcessShot() const {
 
   
   std::unique_ptr<Image<RgbPixel>> image = rgbImageFromHalide(output);
-  //(new Image<RgbPixel>(width, height));
-  //std::unique_ptr<Image<RgbPixel>> image(new Image<RgbPixel>(width, height));
-  //for (int row = 0; row < output.height(); row++) {
-    //for (int col = 0; col < output.width(); col++) {
-
-      //// Note: Halide uses the col, row, channel convention
-      //float r = output(col, row, 0);
-      //float g = output(col, row, 1);
-      //float b = output(col, row, 2);
-
-      //auto& pixel = (*image)(row, col);
-      //pixel.r = r;
-      //pixel.g = g;
-      //pixel.b = b;
-    //}
-  //}
-
   return image;
 
 #else
